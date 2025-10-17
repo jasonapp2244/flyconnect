0e108f7cd3d0b5b4bb080ab75c188cbd3771cec2
jasonapp2244 <israkhan.ik1@gmail.com>
2025-10-16 17:42:04 -0700

    recent code

--- lib/screens/home/edit_profile_screen.dart
+++ lib/screens/home/edit_profile_screen.dart
@@ -1,9 +1,13 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flyconnect/const/colorconstraint.dart';
 import 'package:flyconnect/provider/edit_profile_provider.dart';
+import 'package:flyconnect/screens/widgets/custom_textfield.dart';
+import 'package:flyconnect/utils/responsive.dart';
 
 import 'package:provider/provider.dart';
+import 'dart:io';
 
 class EditProfileScreen extends StatelessWidget {
   const EditProfileScreen({super.key});
@@ -12,110 +16,159 @@ class EditProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final provider = Provider.of<EditProfileProvider>(context);
 
+    // Text controllers for fields
+    final usernameController = TextEditingController(text: provider.username);
+    final emailController = TextEditingController(text: provider.email);
+    final phoneController = TextEditingController(text: provider.phone);
+    final passwordController = TextEditingController(text: provider.password);
+
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: const Text(
-          "Edit Profile",
-          style: TextStyle(color: Colors.white),
-        ),
-        centerTitle: true,
-      ),
-      body: Container(
-        decoration: const BoxDecoration(color: Colors.black),
-        padding: const EdgeInsets.all(16),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              // Profile Image
-              GestureDetector(
-                onTap: provider.pickImage,
-                child: CircleAvatar(
-                  radius: 60,
-                  backgroundImage: provider.pickedImage != null
-                      ? FileImage(File(provider.pickedImage!.path))
-                      : null,
-                  child: provider.pickedImage == null
-                      ? const Icon(
-                          Icons.person,
-                          size: 60,
-                          color: Colors.white54,
-                        )
-                      : null,
-                ),
-              ),
-              const SizedBox(height: 20),
+      body: SizedBox.expand(
+        child: Stack(
+          children: [
+            // 🔹 Full-screen Background Image
+            Positioned.fill(
+              child: Image.asset('assets/images/bg.png', fit: BoxFit.cover),
+            ),
 
-              // Username
-              TextField(
-                decoration: const InputDecoration(
-                  hintText: "Username",
-                  hintStyle: TextStyle(color: Colors.white54),
-                ),
-                style: const TextStyle(color: Colors.white),
-                onChanged: provider.setUsername,
-              ),
-              const SizedBox(height: 16),
+            // 🔹 Content Overlay
+            Container(
+              padding: const EdgeInsets.fromLTRB(16, 100, 16, 16),
 
-              // Email
-              TextField(
-                decoration: const InputDecoration(
-                  hintText: "Email",
-                  hintStyle: TextStyle(color: Colors.white54),
-                ),
-                style: const TextStyle(color: Colors.white),
-                onChanged: provider.setEmail,
-              ),
-              const SizedBox(height: 16),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Profile Image
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        GestureDetector(
+                          onTap: provider.pickImage,
+                          child: CircleAvatar(
+                            radius: 60,
+                            backgroundImage: provider.pickedImage != null
+                                ? FileImage(File(provider.pickedImage!.path))
+                                : null,
+                            child: provider.pickedImage == null
+                                ? const Icon(
+                                    Icons.person,
+                                    size: 60,
+                                    color: Colors.white54,
+                                  )
+                                : null,
+                          ),
+                        ),
+                      ],
+                    ),
 
-              // Phone
-              TextField(
-                decoration: const InputDecoration(
-                  hintText: "Phone Number",
-                  hintStyle: TextStyle(color: Colors.white54),
-                ),
-                style: const TextStyle(color: Colors.white),
-                onChanged: provider.setPhone,
-              ),
-              const SizedBox(height: 16),
+                    const SizedBox(height: 30),
+                    Text(
+                      'Username',
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.6),
+                        fontSize: Responsive.sp(13),
+                      ),
+                    ),
+                    SizedBox(height: 5),
 
-              // Password
-              TextField(
-                decoration: const InputDecoration(
-                  hintText: "Password",
-                  hintStyle: TextStyle(color: Colors.white54),
-                ),
-                obscureText: true,
-                style: const TextStyle(color: Colors.white),
-                onChanged: provider.setPassword,
-              ),
-              const SizedBox(height: 30),
+                    // Username
+                    CustomTextField(
+                      label: "Username",
+                      icon: Icons.person,
+                      controller: usernameController,
+                    ),
+                    const SizedBox(height: 16),
 
-              // Update Button
-              ElevatedButton(
-                onPressed: () {
-                  try {
-                    provider.updateProfile();
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text("Profile Updated")),
-                    );
-                  } catch (e) {
-                    ScaffoldMessenger.of(
-                      context,
-                    ).showSnackBar(SnackBar(content: Text(e.toString())));
-                  }
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.red,
-                  minimumSize: const Size(double.infinity, 48),
-                ),
-                child: const Text(
-                  "Update Profile",
-                  style: TextStyle(color: Colors.white),
+                    // Email
+                    Text(
+                      'Email',
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.6),
+                        fontSize: Responsive.sp(13),
+                      ),
+                    ),
+                    SizedBox(height: 5),
+
+                    CustomTextField(
+                      label: "Email",
+                      icon: Icons.email,
+                      controller: emailController,
+                    ),
+                    const SizedBox(height: 16),
+
+                    // Phone
+                    Text(
+                      'Phone Number',
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.6),
+                        fontSize: Responsive.sp(13),
+                      ),
+                    ),
+                    SizedBox(height: 5),
+
+                    CustomTextField(
+                      label: "Phone Number",
+                      icon: Icons.phone,
+                      controller: phoneController,
+                    ),
+                    const SizedBox(height: 16),
+
+                    // Password
+                    Text(
+                      'Password',
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.6),
+                        fontSize: Responsive.sp(13),
+                      ),
+                    ),
+                    SizedBox(height: 5),
+
+                    CustomTextField(
+                      label: "Password",
+                      icon: Icons.lock,
+                      controller: passwordController,
+                      obscureText: true,
+                      suffixIcon: Icons.visibility_off,
+                    ),
+                    const SizedBox(height: 30),
+
+                    // Update Button
+                    ElevatedButton(
+                      onPressed: () {
+                        provider.setUsername(usernameController.text);
+                        provider.setEmail(emailController.text);
+                        provider.setPhone(phoneController.text);
+                        provider.setPassword(passwordController.text);
+
+                        try {
+                          provider.updateProfile();
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text("Profile Updated")),
+                          );
+                        } catch (e) {
+                          ScaffoldMessenger.of(
+                            context,
+                          ).showSnackBar(SnackBar(content: Text(e.toString())));
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: ColorConstraint.redColor,
+                        minimumSize: const Size(double.infinity, 48),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      child: const Text(
+                        "Update Profile",
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
